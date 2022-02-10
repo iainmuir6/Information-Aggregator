60526d2fa3ea2d6850c0875a89bff449189e9374
Iain Muir <59845382+iainmuir6@users.noreply.github.com>
2022-02-10 16:24:37 -0500

    Update automator.sh

--- automator.sh
+++ automator.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# Switch to Correct Directory
 cd /Users/iainmuir/PycharmProjects/Desktop/PersonalProjects/Information-Aggregator || exit
+
+# Activate Virtual Environment
 source bin/activate
+
+# Connect to Datapane API
 datapane login --token=55010cebc170ecfbeddb82838c360776bf36f6be
+
+# Run Jupyter Notebook
 jupyter nbconvert --to notebook --execute aggregator.ipynb --output aggregator.ipynb
+
+# Commit updated run logg
+git commit -m "daily run; updating log..."
+git push
+
+# Deactivate Virtual Environment
 deactivate
