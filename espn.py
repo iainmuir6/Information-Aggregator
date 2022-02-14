a4fe23f8b12e06286015bdb2f1f3610f2b8cd0c2
Iain Muir <59845382+iainmuir6@users.noreply.github.com>
2022-02-14 16:36:23 -0500

    add schedule to espn; css to functions

--- espn.py
+++ espn.py
@@ -6,16 +6,20 @@
     Project:
 """
 
+# Import Libraries
 from errors import ErrorHandler, get_error_info
-from itertools import zip_longest
 from constants import SPORT_LOGOS
 from datetime import timedelta
+from functools import partial
 import datapane as dp
 import pandas as pd
+import numpy as np
 import datetime
 import requests
 import json
-import math
+
+# Global Variables
+left = True
 
 
 # ------------------- ESPN NEWS --------------------
@@ -33,7 +37,7 @@ def format_news(article):
     return f"""
         <html>
             <h2>
-                <a href="{links['web']['href']}">{headline}</a>
+                <a href="{links['web']['href']}" target="_blank">{headline}</a>
             </h2>
             {byline}
             <p font-size="10" color="gray">
@@ -65,22 +69,22 @@ def news(sport):
     return html
 
 
-# ----------- ESPN STATS/LEADERS/YOUTUBE -----------
-def format_stats(stats):
+# -------------- FORMATTING FUNCTIONS --------------
+def format_stats(stat):
     """
 
-    :param stats:
+    :param stat:
     :return:
     """
-    stat, home, away = stats
-    home = int(home) if isinstance(home, float) and ".0" in str(home) else home
-    away = int(away) if isinstance(away, float) and ".0" in str(away) else away
+    home, away = stat
+    home, away = float(home), float(away)
+    home = int(home) if ".0" in str(home) else home
+    away = int(away) if ".0" in str(away) else away
 
-    return f"""
-        <div class='stat'><b>{stat}</b></div>
-        <div class='value_{'high' if home > away else 'even' if home == away else 'low'}'>{home}</div>
-        <div class='value_{'high' if away > home else 'even' if home == away else 'low'}'>{away}></div>
-    """.strip()
+    colors = ['true ', 'false '] if home > away else ['false ', 'false '] if home == away else ['false ', 'true ']
+    ret = [home, away]
+    ret.extend(colors)
+    return ret
 
 
 def format_leaders(leaders):
@@ -108,11 +112,136 @@ def format_leaders(leaders):
     """.strip()
 
 
+def leader_css():
+    """
+
+    :return:
+    """
+    return """
+        <style type='text/css'>
+            .header {
+                font-size:16px;
+                text-align:center;
+                width: 50%;
+                padding: 20px;
+            }
+            .player {
+                font-size:14px;
+                text-align:center;
+                width: 50%;
+                margin: auto;
+            }
+            .stat {
+                font-size:12px;
+                text-align:center;
+                width:50%;
+                margin:auto;
+                color:#808080;
+            }
+            .value_high {
+                font-size:16px;
+                font-weight:bold;
+                text-align:center;
+                width: 50%;
+                margin: auto;
+                color:#228B22;
+            }
+            .value_low {
+                font-size:16px;
+                font-weight:bold;
+                text-align:center;
+                width: 50%;
+                margin: auto;
+            }
+            .value_even {
+                font-size:16px;
+                font-weight:bold;
+                text-align:center;
+                width: 50%;
+                margin: auto;
+            }
+            .grid-container {
+                display: grid;
+                grid-template-columns: auto auto auto auto auto auto auto;
+            }
+        </style>
+    """.strip()
+
+
+def game_css(left_):
+    """
+
+    :param left_:
+    :return:
+    """
+    return """
+        <style type='text/css'>
+            .info_winner {
+                text-align:""" + ('right' if left_ else 'left') + """;
+                font-size:16px;
+                color:#000000;
+            }
+            .info_loser {
+                text-align:""" + ('right' if left_ else 'left') + """;
+                font-size:16px;
+                color:#808080;
+            }
+            .score_winner {
+                text-align:""" + ('left' if left_ else 'right') + """;
+                font-size:20px;
+                color:#000000;
+            }
+            .score_loser {
+                text-align:""" + ('left' if left_ else 'right') + """;
+                font-size:20px;
+                color:#808080;
+            }
+            .info_winner span {
+                font-size:12px;
+                color:#808080;
+            }
+            .info_loser span {
+                font-size:12px;
+                color:#808080;
+            }
+            .image {
+                display:flex;
+                justify-content:center;
+                align-items:center;
+            }
+            .images img {
+                margin-left:5px;
+                margin-right:5px;
+            }
+            a {
+                font-size:12px;
+                text-decoration:none;
+                color:#000000;
+            }
+            .grid-container {
+                display: grid;
+                grid-template-columns: auto auto auto;
+                border-top: 0px solid black;
+                border-left: 0px solid black;
+            }
+            .grid-container > div {
+                border-bottom: 0px;
+                border-right: 0px;
+            }
+            .grid-item7 {
+                grid-column-start: 1;
+                grid-column-end: 4
+            }
+        </style>
+    """.strip()
+
+
 # -------------- ESPN SCORES/SCHEDULE --------------
-def parse_team(t):
+def parse_team(t, upcoming):
     """
 
     :param t:
+    :param upcoming:
     :return:
     """
     global left
@@ -138,11 +267,11 @@ def parse_team(t):
         rank = str(rank) if rank <= 25 else ''
 
     stats = None if stats == [] else stats
-    if stats is not None:
+    if stats is not None and not upcoming:
         stats = pd.DataFrame(stats)[['abbreviation', 'displayValue']].loc[1:]
         stats.columns = ['Stat', short_display]
 
-    if leaders is not None:
+    if leaders is not None and not upcoming:
         leaders = [
             [
                 lead['abbreviation'], lead['leaders'][0]['value'],
@@ -156,17 +285,17 @@ def parse_team(t):
     try:
         record = records[0]['summary']
     except (TypeError, KeyError):
-        print(
-            ErrorHandler("Missing Team Record; OK.", *get_error_info())
-        )
+        ErrorHandler("Missing Team Record; OK.", *get_error_info())
         record = ''
     try:
         conf_record = records[3]['summary']
     except (TypeError, KeyError, IndexError):
-        print(
-            ErrorHandler("Missing Team Conference Record; OK.", *get_error_info())
-        )
+        ErrorHandler("Missing Team Conference Record; OK.", *get_error_info())
         conf_record = None
+
+    if upcoming:
+        winner = True
+        score = ''
 
     info = """
             <div class="grid-item"> 
@@ -204,10 +333,11 @@ def parse_team(t):
     return g, stats, leaders
 
 
-def format_scores(score):
+def format_scores(score, upcoming):
     """
 
     :param score:
+    :param upcoming:
     :return:
     """
     # TODO Make an Iterable
@@ -223,79 +353,23 @@ def format_scores(score):
     competitors = c['competitors']
     venue = f"{venue['fullName']} ({', '.join(venue['address'].values())})"
 
-    results = list(map(parse_team, competitors))
+    results = list(map(
+        lambda comp: parse_team(comp, upcoming), competitors
+    ))
     home, away = results
 
     game_stats = """
         <div class="grid-item7"> 
             <center>
-                <a href='""" + link + """'>
+                <a href='""" + link + """' target="_blank">
                     <i>""" + venue + """ <br>  Attendance: """ + attendance + """</i>
                 </a>
             </center>
         </div>
     """.strip()
-
     html = """
         <html>
-            <style type='text/css'>
-                .info_winner {
-                    text-align:""" + ('right' if left else 'left') + """;
-                    font-size:16px;
-                    color:#000000;
-                }
-                .info_loser {
-                    text-align:""" + ('right' if left else 'left') + """;
-                    font-size:16px;
-                    color:#808080;
-                }
-                .score_winner {
-                    text-align:""" + ('left' if left else 'right') + """;
-                    font-size:20px;
-                    color:#000000;
-                }
-                .score_loser {
-                    text-align:""" + ('left' if left else 'right') + """;
-                    font-size:20px;
-                    color:#808080;
-                }
-                .info_winner span {
-                    font-size:12px;
-                    color:#808080;
-                }
-                .info_loser span {
-                    font-size:12px;
-                    color:#808080;
-                }
-                .image {
-                    display:flex;
-                    justify-content:center;
-                    align-items:center;
-                }
-                .images img {
-                    margin-left:5px;
-                    margin-right:5px;
-                }
-                a {
-                    font-size:12px;
-                    text-decoration:none;
-                    color:#000000;
-                }
-                .grid-container {
-                    display: grid;
-                    grid-template-columns: auto auto auto;
-                    border-top: 0px solid black;
-                    border-left: 0px solid black;
-                }
-                .grid-container > div {
-                    border-bottom: 0px;
-                    border-right: 0px;
-                }
-                .grid-item7 {
-                    grid-column-start: 1;
-                    grid-column-end: 4
-                }
-            </style>
+            """ + game_css(left) + """
             <div class="grid-container">
                 """ + home[0] + """
                 """ + away[0] + """
@@ -305,8 +379,11 @@ def format_scores(score):
         """.strip()
     game = dp.HTML(html)
 
-    blocks = list()
+    if upcoming:
+        left = not left
+        return game
 
+    blocks = list()
     home_team, away_team = name.split(' at ')
 
     stats_h, stats_a = home[1], away[1]
@@ -314,57 +391,35 @@ def format_scores(score):
         # TODO Actual Join...
         stats = stats_a
         stats[stats_h.columns[1]] = stats_h.iloc[:, 1]
-
-        # stats = stats.apply(
-        #     format_stats,
-        #     axis=1
-        # )
-        # stats = """
-        #     <html>
-        #         <style type='text/css'>
-        #             .header {
-        #                 font-size:12px;
-        #             }
-        #             .stat {
-        #                 font-size:12px;
-        #             }
-        #             .value_high {
-        #                 font-size:12px;
-        #                 font-weight:bold;
-        #                 color:#023020;
-        #             }
-        #             .value_low {
-        #                 font-size:12px;
-        #             }
-        #             .value_even {
-        #                 font-size:12px;
-        #             }
-        #             .grid-container {
-        #                 display: grid;
-        #                 grid-template-columns: auto auto auto;
-        #                 border-top: 0px solid black;
-        #                 border-left: 0px solid black;
-        #             }
-        #             .grid-container > div {
-        #                 border-bottom: 0px;
-        #                 border-right: 0px;
-        #             }
-        #         </style>
-        #         <div class="grid-container">
-        #             <div class="header"><b>Stat</b></div>
-        #             <div class="header"><b>""" + home_team.strip() + """</b></div>
-        #             <div class="header"><b>""" + away_team.strip() + """</b></div>
-        #             """ + "\n".join(stats) + """
-        #         </div>
-        #     </html>
-        # """.strip()
-        # print(stats)
-
-        stats = dp.Table(
-            stats
+        stats = stats.drop_duplicates(subset=['Stat']).set_index("Stat")
+        formatted = stats.apply(
+            format_stats,
+            axis=1,
+            result_type='expand'
         )
+        formatted_stats = pd.concat(
+            [formatted[0], formatted[1]],
+            axis=1
+        )
+        formatted_stats.columns = stats.columns
+        cell_color = pd.concat(
+            [formatted[2], formatted[3]],
+            axis=1
+        )
+        cell_color.columns = stats.columns
+        formatted_stats.style.set_table_styles([
+            # Cell Hover
+            {'selector': 'td:hover', 'props': [('background-color', '#ffffb3')]},
+            # Create Internal CSS Classes
+            {'selector': '.true', 'props': 'background-color: #e6ffe6;'},
+            {'selector': '.false', 'props': 'background-color: #ffe6e6;'},
+            # Center/Bold Table Text
+            {'selector': 'td', 'props': 'text-align: center; font-weight: bold;'}
+        ])
+        formatted_stats.style.set_td_classes(cell_color)
+        formatted_stats = dp.Table(formatted_stats)
         blocks.extend(
-            [dp.Divider(), stats]
+            [dp.Divider(), formatted_stats]
         )
 
     leaders_h, leaders_a = home[2], away[2]
@@ -379,60 +434,14 @@ def format_scores(score):
         )
         html = """
             <html>
-                <style type='text/css'>
-                    .header {
-                        font-size:16px;
-                        text-align:center;
-                        width: 50%;
-                        padding: 20px;
-                    }
-                    .player {
-                    	font-size:14px;
-                        text-align:center;
-                        width: 50%;
-                        margin: auto;
-                    }
-                    .stat {
-                        font-size:12px;
-                        text-align:center;
-                        width:50%;
-                        margin:auto;
-                        color:#808080;
-                    }
-                    .value_high {
-                        font-size:16px;
-                        font-weight:bold;
-                        text-align:center;
-                        width: 50%;
-                        margin: auto;
-                        color:#228B22;
-                    }
-                    .value_low {
-                        font-size:16px;
-                        font-weight:bold;
-                        text-align:center;
-                        width: 50%;
-                        margin: auto;
-                    }
-                    .value_even {
-                        font-size:16px;
-                        font-weight:bold;
-                        text-align:center;
-                        width: 50%;
-                        margin: auto;
-                    }
-                    .grid-container {
-                        display: grid;
-                        grid-template-columns: auto auto auto auto auto auto auto;
-                    }
-                </style>
+                """ + leader_css() + """
                 <div class="grid-container">
                     <div></div>
-                    <div class="header"><b><u>""" + home_team.strip() + """</u></b></div>
+                    <div class="header"><b><u>""" + away_team.strip() + """</u></b></div>
                     <div></div>
                     <div class="header"><b></b></div>
                     <div></div>
-                    <div class="header"><b><u>""" + away_team.strip() + """</u></b></div>
+                    <div class="header"><b><u>""" + home_team.strip() + """</u></b></div>
                     <div></div>
                     """ + "\n".join(leaders) + """
                 </div>
@@ -446,32 +455,36 @@ def format_scores(score):
 
     left = not left
 
-    # return game
-    return dp.Group(
-        game,
-        dp.Toggle(
-            *blocks,
-            label=f'Analysis: {short_name}'
+    if len(blocks) == 0:
+        print(
+            ErrorHandler(f"No Stats/Leaders for {short_name}; OK.", "CustomError", "espn.py", "~400")
         )
-    )
+        return game
+    else:
+        return dp.Group(
+            game,
+            dp.Toggle(
+                *blocks,
+                label=f'Analysis: {short_name}'
+            )
+        )
 
 
-def scores(s):
+def scores(s, upcoming=False):
     """
 
-    :param s: individual game result
+    :param s: sport name and code
+    :param upcoming: result or upcoming game
     :return: sport-labeled dp.Group object
-
-    # TODO Nested dp.Group (r=2, c=1): Inner Group and In-Depth Game Breakdown (stats/leaders/YouTube)
     """
     global left
 
     sport, code = s
 
     today = datetime.date.today()
-    yesterday = (today - timedelta(days=1)).strftime('%Y%m%d')
+    date = (today - timedelta(days=1)).strftime('%Y%m%d') if not upcoming else today.strftime('%Y%m%d')
 
-    api_link = f"http://site.api.espn.com/apis/site/v2/sports/{code}/scoreboard?dates={yesterday}"
+    api_link = f"http://site.api.espn.com/apis/site/v2/sports/{code}/scoreboard?dates={date}"
 
     with requests.get(api_link) as r:
         resp = json.loads(
@@ -481,27 +494,42 @@ def scores(s):
 
     left = True
     games = list(scoreboard.apply(
-        format_scores,
+        lambda row: format_scores(row, upcoming),
         axis=1
     ))
 
     return games
 
 
-def group_sport(sport, games):
+def group_sport(sport, results, upcoming):
     """
 
     :param sport
-    :param games
+    :param results
+    :param upcoming
     :return
     """
 
-    if len(games) == 0:
+    if len(results) == 0:
         return None
 
-    game_group = dp.Group(
-        *games,
-        columns=2
+    results_group = dp.Group(
+        *results,
+        columns=2,
+        label='Scores'
+    )
+    upcoming_group = dp.Group(
+        *upcoming,
+        columns=2,
+        label='Schedule'
+    )
+    highlights_group = dp.Text(
+        'TODO! YouTube Highlights',
+        label='Highlights'
+    )
+    standings_group = dp.Text(
+        'TODO! Standings',
+        label='Standings'
     )
 
     logo = SPORT_LOGOS[sport]
@@ -517,6 +545,47 @@ def group_sport(sport, games):
     return dp.Group(
         title,
         dp.Divider(),
-        game_group,
+        dp.Select(
+            results_group,
+            upcoming_group,
+            highlights_group,
+            standings_group
+        ),
         label=sport
     )
+
+
+# -------------- ESPN SCORES/SCHEDULE --------------
+def standings(s):
+    """
+
+    :param s: sport name and code
+    :return:
+    """
+
+    sport, code = s
+    api_link = f'http://site.api.espn.com/apis/site/v2/sports/{sport}/teams'
+    with requests.get(api_link) as r:
+        resp = json.loads(
+            r.content.decode('utf-8')
+        )
+        leagues = resp['sports'][0]['leagues']
+
+    records = [
+        (
+            team['team']['name'],
+            {
+                stat['name']: stat['value']
+                for stat in team['team']['record']['items'][0]['stats']
+            }
+        )
+        for league in leagues
+        for team in league['teams']
+    ]
+    records = np.array(records).flatten().tolist()
+    teams = records[::2]
+    records = pd.DataFrame(
+        records[1::2],
+        index=teams
+    )
+    return records
